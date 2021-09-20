97ef2df0dd69b5b41f039f6b88bcac2d020af320
robertthome <remedy42290@gmail.com>
2021-09-20 10:03:10 -0700

    user controllers(0)

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,3 +8,29 @@ const GetUsers = async (req, res) => {
     throw error
   }
 }
+
+const GetUserProfile = async (req, res) => {
+  try {
+    const userProfile = await User.findByPk(req.params.user_id, {
+      include: [{ model: Comments, as: 'comments' }]
+    })
+    res.send(userProfile)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreateUser = async (req, res) => {
+  try {
+    let name = req.body.content.userName
+    let email = req.body.content.email
+    let body = {
+      userName: name,
+      email: emails,
+      created: new Date(),
+      updated: new Date()
+    }
+  } catch (error) {
+    throw error
+  }
+}
