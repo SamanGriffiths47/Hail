ee28be5b780593d564aae533b7cc4669dd8e718a
robertthome <remedy42290@gmail.com>
2021-09-20 11:32:12 -0700

    updated controlls and routes

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -1,10 +1,10 @@
 const Router = require('express').Router()
 const controller = require('../controllers/CommentController')
 
-Router.get('/', controller.GetComments)
-Router.get('/view/:comment_id', controller.GetCommentDetails)
+Router.get('/', controller.FindComments)
+Router.get('/view/:comment_id', controller.FindCommentById)
 Router.post('/:user_id', controller.CreateComment)
-Router.put('/:comment_id', controller.UpdateComment)
+Router.put('/:comment_id', controller.UpdateComments)
 Router.delete('/del/:comment_id', controller.DeleteComment)
 
 module.exports = Router
