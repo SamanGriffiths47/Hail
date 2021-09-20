ec96e035c4334722acd8077d05ac0d7154123d4e
Zitai Huang <mywpiedu@gmail.com>
2021-09-20 16:07:01 -0500

    auth backend

---
+++ controllers/AuthController.js
@@ -0,0 +1,72 @@
+const { User } = require('../models')
+const middleware = require('../middleware')
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { email: req.body.email },
+      raw: true
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(
+        user.password_digest,
+        req.body.password
+      ))
+    ) {
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    throw error
+  }
+}
+
+const Register = async (req, res) => {
+  try {
+    const { email, name, password, city_state, country } = req.body
+    let passwordDigest = await middleware.hashPassword(password)
+    const user = await User.create({
+      admin: false,
+      email,
+      passwordDigest,
+      name,
+      city_state,
+      country
+    })
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdatePassword = async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } })
+    if (
+      user &&
+      (await middleware.comparePassword(
+        user.dataValues.passwordDigest,
+        req.body.oldPassword
+      ))
+    ) {
+      let passwordDigest = await middleware.hashPassword(req.body.newPassword)
+
+      await user.update({ passwordDigest })
+      return res.send({ status: 'Success', msg: 'Password Updated' })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Invalid Credentials' })
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  Login,
+  Register,
+  UpdatePassword
+}
