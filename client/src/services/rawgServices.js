bf3085a739aa1bb5b8aef2c289111976b91daecd
Sam'an <samangriffiths47@gmail.com>
2021-09-20 16:57:26 -0500

    rawg game query

--- client/src/services/rawgServices.js
+++ client/src/services/rawgServices.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 export default async function grabGames() {
   try {
     const res = await axios.get(`${RAWG_URL}/games?key=${RAWG_KEY}`)
+    console.log(res)
     return res.data.results
   } catch (error) {
     throw error
