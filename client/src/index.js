49ef0deb6988b99c37c8df2546a1b42d7eb09803
Sam'an <samangriffiths47@gmail.com>
2021-09-20 14:37:09 -0500

    server fix

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import './CSS/index.css'
 import App from './App'
 import { Provider } from 'react-redux'
-import store from './Redux/store'
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
