9ec1d05c2ba28c0fdb187cee8cc9fd867ecc93a2
Sam'an <samangriffiths47@gmail.com>
2021-09-24 14:10:10 -0500

    un and reinstall express

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 
 const AppRouter = require('./routes/AppRouter')
 
@@ -10,8 +11,6 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const path = require('path')
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.use('/api', AppRouter)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
